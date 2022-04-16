77d38b6c41f321a7a5959a290344f4655400f2c5
Arunk <arunkeengineer@gmail.com>
2022-04-16 16:02:28 +0530

    374. Guess Number Higher or Lower

---
+++ src/easy/binarysearch/GuessNumberHigherorLower.java
@@ -0,0 +1,58 @@
+/**
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+    -1: Your guess is higher than the number I picked (i.e. num > pick).
+    1: Your guess is lower than the number I picked (i.e. num < pick).
+    0: your guess is equal to the number I picked (i.e. num == pick).
+
+Return the number that I picked.
+
+Example 1:
+Input: n = 10, pick = 6
+Output: 6
+
+Example 2:
+Input: n = 1, pick = 1l
+Output: 1
+
+Example 3:
+Input: n = 2, pick = 1
+Output: 1
+
+Constraints:
+    1 <= n <= 2^31 - 1
+    1 <= pick <= n
+ */
+
+package easy.binarysearch;
+
+public class GuessNumberHigherorLower {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 10;
+		System.out.println(guessNumber(n));
+	}
+	
+	static int guessNumber(int n) {
+        int low = 1, high = n;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            int no = guess(mid);
+            if(no == 0) return mid;
+            else if(no == 1) low = mid+1;
+            else high = mid-1;
+        }
+        return -1;
+    }
+
+	private static int guess(int mid) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
