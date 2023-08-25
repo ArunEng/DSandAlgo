124104f63cd08b753b62f3e2e7d4c2d5fb0c556a
arun <arunkeengineer@gmail.com>
2023-08-25 16:12:05 +0530

    274. H-Index

---
+++ src/medium/array/HIndex.java
@@ -0,0 +1,48 @@
+package medium.array;
+
+/*
+Given an array of integers citations where citations[i] is the number of citations a researcher received for their ith paper, return the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: The h-index is defined as the maximum value of h such that the given researcher has published at least h papers that have each been cited at least h times.
+
+Example 1:
+Input: citations = [3,0,6,1,5]
+Output: 3
+Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively.
+Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, their h-index is 3.
+
+Example 2:
+Input: citations = [1,3,1]
+Output: 1
+
+Constraints:
+n == citations.length
+1 <= n <= 5000
+0 <= citations[i] <= 1000
+* */
+public class HIndex {
+    public static void main(String[] args) {
+        int[] citations = {1,100, 200, 500};
+        System.out.println(hIndex(citations));
+    }
+
+    private static int hIndex(int[] citations) {
+        int n = citations.length;
+        int[] count = new int[n + 1];
+        for (int citation : citations) {
+            if (citation >= n) {
+                count[n]++;
+            } else {
+                count[citation]++;
+            }
+        }
+        int hi = 0;
+        for (int i = n; i >= 0; i--) {
+            hi += count[i];
+            if (hi >= i) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
